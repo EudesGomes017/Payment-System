7389a7a5852210041a0cb34e91d973d8486f4855
EudesGomes017 <eudesgomes017@gmail.com>
2025-01-13 12:15:41 -0300

    Initial commit

---
+++ src/test/java/com/EudesGomes017/payment_system/PaymentSystemApplicationTests.java
@@ -0,0 +1,13 @@
+package com.EudesGomes017.payment_system;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class PaymentSystemApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
